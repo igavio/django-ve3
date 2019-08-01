dae59901ad94b54d4eb38588438323bcf843d322
Gaviotis Ioannis <gaviotis@ggb.gr>
2019-08-01 18:03:21 +0300

    final version

--- dbapp/urls.py
+++ dbapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ArxikiView, PeriView
+from .views import ArxikiView, PeriView, EnaParapono
 
 urlpatterns = [
     path('', ArxikiView.as_view(), name='arxikiSelida'),
     path('peri', PeriView.as_view(), name='periSelida'),
+    path('ena_par/<int:pk>/', EnaParapono.as_view(), name='ena_parSelida'),
 ]
--- dbapp/views.py
+++ dbapp/views.py
@@ -1,10 +1,16 @@
-from django.views.generic import ListView, TemplateView
+from django.views.generic \
+    import ListView, TemplateView, DetailView
 from .models import Parapono
 
 class ArxikiView(ListView):
     model = Parapono
-    template_name = "arxiki.html"
-    context_object_name = "lista_paraponon"
+    template_name = 'arxiki.html'
+    context_object_name = 'lista_paraponon'
 
 class PeriView(TemplateView):
-    template_name = "peri.html"
+    template_name = 'peri.html'
+
+class EnaParapono(DetailView):
+    model = Parapono
+    template_name = 'ena_par.html'
+    context_object_name = 'ena_parapono'
